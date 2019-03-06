8185aff0041c8fdc809f347e891531745ed90e20
Mubasher Ahmad <mubasherahmad012345@gmail.com>
2019-03-06 22:09:41 +0500

    A Person can be registered.

---
+++ DBMSL_ProjectA/DBMSL_ProjectA/DatabaseConnection.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data.Sql;
+using System.Data;
+
+namespace DBMSL_ProjectA
+{
+    class DatabaseConnection
+    {
+        static String connectionURL = "Data Source=MUSHI\\MUSHISQL;Initial Catalog=ProjectA;Integrated Security=True; MultipleActiveResultSets=true";
+                            
+        static SqlConnection connectionObject;
+        static SqlCommand command;
+
+        public DatabaseConnection()
+        {
+
+        }
+
+        public static void setURL(string serverName, string dbName, string username, string password)
+        {
+            connectionURL = "Data Source = " + serverName + "; Initial Catalog = " + dbName + "; User ID = " + username + ";"
+                    + " Password = " + password + "; MultipleActiveResultSets = True";
+        }
+
+
+        
+        public static bool start()
+        {
+
+            connectionObject = new SqlConnection(connectionURL);
+            try
+            {
+                connectionObject.Open();
+                return true;
+            }
+            catch (SqlException)
+            {
+                return false;
+            }
+
+
+        }
+        
+        public static void createStatement(string query)
+        {
+            command = new SqlCommand(query);
+            command.Connection = connectionObject;
+        }
+
+
+        public static SqlDataReader getData()
+        {
+            return command.ExecuteReader();
+        }
+
+        public static int performAction()
+        {
+            return command.ExecuteNonQuery();
+        }
+
+        public static int executeScalar()
+        {
+            return (int)command.ExecuteScalar();
+        }
+
+        public static void close()
+        {
+            if (connectionObject.State == ConnectionState.Open)
+                connectionObject.Close();
+        }
+
+        public static SqlConnection getConnectionObj()
+        {
+            return connectionObject;
+        }
+
+    }
+}
---
+++ DBMSL_ProjectA/DBMSL_ProjectA/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data.Sql;
+using System.Collections;
+
+namespace DBMSL_ProjectA
+{
+    public partial class Form1 : Form
+    {
+
+        List<string> Values = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool IsConnnected = DatabaseConnection.start();
+            if (IsConnnected)
+            {
+                MessageBox.Show("Connected");
+            }
+            else
+            {
+                MessageBox.Show("Error");
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            DatabaseConnection.createStatement("INSERT INTO Person (FirstName, LastName, Contact, Email, DateOfBirth, Gender)" + 
+            "VALUES('Mushi', 'Ahmad', '031342431147', 'mushi.ahmad1101@gmail.com', 1998 - 9 - 30, 1); ");
+            DatabaseConnection.performAction();
+            
+            /* SqlDataReader reader = DatabaseConnection.getData();
+            textBox1.Text = "Result";
+            while (reader.Read())
+            {
+                Values.Add(reader["Value"].ToString());
+            }
+            DatabaseConnection.close();
+            foreach (string s in Values)
+            {
+                richTextBox1.Text += s;
+                richTextBox1.Text += "\n";
+
+            }*/
+            
+            DatabaseConnection.createStatement("Select @@identity as id from Person");
+            SqlDataReader reader = DatabaseConnection.getData();
+            string id = "0";
+            while (reader.Read())
+            {
+               id = (reader["id"].ToString());
+            }
+            MessageBox.Show(id);
+            
+            DatabaseConnection.createStatement("INSERT INTO Student (Id, RegistrationNo) VALUES( "+ id +",'2016-CS-312') ");
+            DatabaseConnection.performAction();
+            
+            
+        }
+
+    }
+}
